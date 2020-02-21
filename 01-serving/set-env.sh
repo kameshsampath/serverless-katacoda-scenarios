b330159aeb74de6f9212ac362e7a3b4ad0495123
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-21 23:59:08 +0530

    using install yaml from github repo

--- 01-serving/set-env.sh
+++ 01-serving/set-env.sh
@@ -4,20 +4,18 @@ set -o pipefail
 
 echo "Installing OpenShift Serverless"
 
-ls -ltr /etc/knative-serving
+oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/redhat-operators-csc.yaml
 
-# [[ -f /root/redhat-operators-csc.yaml ]] && oc create -f /root/redhat-operators-csc.yaml
+sleep 30
 
-# sleep 30
+! oc adm new-project knative-serving
 
-# ! oc adm new-project knative-serving
+oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/knative-serving/subscription.yaml 
 
-# [[ -f /root/subscription.yaml ]] && oc apply -f /root/subscription.yaml 
+sleep 120
 
-# sleep 120
+oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/knative-serving/cr.yaml 
 
-# [[ -f /root/knative-serving/cr.yaml ]] && oc apply -f /root/knative-serving/cr.yaml
+echo "Waiting for Knative Serving Pods to comeup"
 
-# echo "Waiting for Knative Serving Pods to comeup"
-
-# sleep 5; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+sleep 5; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
