a5051fe4d5f891e07c8adfd3cbab0e0c987cc8bf
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-21 23:12:37 +0530

    moving install scripts to set-env.sh

--- 01-serving/env-init.sh
+++ 01-serving/env-init.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-oc create -f "~/redhat-operators-csc.yaml" 
-sleep 120
-! oc adm new-project knative-serving
-oc apply -f "~/subscription.yaml" 
-sleep 120
-oc apply -f "~/knative-serving/cr.yaml"
+echo "Environment init"
--- 01-serving/set-env.sh
+++ 01-serving/set-env.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-# TODO need to install some tools
+echo "Setting up knative"
 
-echo "Hello World!"
+oc create -f "~/redhat-operators-csc.yaml" 
+sleep 120
+! oc adm new-project knative-serving
+oc apply -f "~/subscription.yaml" 
+sleep 120
+oc apply -f "~/knative-serving/cr.yaml"
+
+sleep 5; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
