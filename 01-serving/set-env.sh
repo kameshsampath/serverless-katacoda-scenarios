c4d68026f92e0295f9756a996448ec05382f0422
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-21 23:18:20 +0530

    fixing setup script

--- 01-serving/set-env.sh
+++ 01-serving/set-env.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-echo "Setting up knative"
+set -eu
 
-oc create -f "~/redhat-operators-csc.yaml" 
+set -o pipefail 
+
+NO_COLOR=${NO_COLOR:-""}
+if [ -z "$NO_COLOR" ]; then
+  header=$'\e[1;33m'
+  reset=$'\e[0m'
+else
+  header=''
+  reset=''
+fi
+
+function header_text {
+  echo "$header$*$reset"
+}
+
+header_text "Setting up knative"
+
+ls -ltr ~/
+[[ -f ~/redhat-operators-csc.yaml ]] && oc create -f ~/redhat-operators-csc.yaml
 sleep 120
 ! oc adm new-project knative-serving
-oc apply -f "~/subscription.yaml" 
+[[ -f ~/subscription.yaml ]] && oc apply -f ~/subscription.yaml 
 sleep 120
-oc apply -f "~/knative-serving/cr.yaml"
+[[ -f ~/knative-serving/cr.yaml ]] && oc apply -f ~/knative-serving/cr.yaml
+
+header_text "Waiting for Knative Serving Pods to comeup"
 
 sleep 5; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
