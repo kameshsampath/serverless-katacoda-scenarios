ddc62ff9b880889146d1fb973286f6daf8396997
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-22 00:06:57 +0530

    moving install to env-init.sh

--- 01-serving/env-init.sh
+++ 01-serving/env-init.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 echo "Environment init"
-mkdir -p /etc/knative-serving
+
+set -o pipefail 
+
+echo "Installing OpenShift Serverless"
+
+oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/redhat-operators-csc.yaml
+
+sleep 30
+
+! oc adm new-project knative-serving
+
+oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/knative-serving/subscription.yaml 
+
+sleep 60
+
+oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/knative-serving/cr.yaml 
+
+sleep 30
--- 01-serving/set-env.sh
+++ 01-serving/set-env.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-set -o pipefail 
-
-echo "Installing OpenShift Serverless"
-
-oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/redhat-operators-csc.yaml
-
-sleep 30
-
-! oc adm new-project knative-serving
-
-oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/knative-serving/subscription.yaml 
-
-sleep 120
-
-oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/knative-serving/cr.yaml 
-
-echo "Waiting for Knative Serving Pods to comeup"
+echo "Check Knative Serving Pods are up and Running"
 
 sleep 5; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
