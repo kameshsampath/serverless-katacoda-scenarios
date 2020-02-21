5d1a176109bda44ebb907d85d9e521d77ab6cd5e
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-22 00:15:18 +0530

    increase time to check install status

--- 01-serving/set-env.sh
+++ 01-serving/set-env.sh
@@ -2,4 +2,4 @@
 
 echo "Check Knative Serving Pods are up and Running"
 
-sleep 20; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 10; done
+sleep 150; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 20; done
