68465a280c2d37fccb45c9c1607a03a37c294f35
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-02-22 10:35:41 +0530

    script fixes

--- 01-serving/env-init.sh
+++ 01-serving/env-init.sh
@@ -18,3 +18,6 @@ sleep 60
 oc create -f https://raw.githubusercontent.com/redhat-developer-demos/guru-night/master/config/knative-serving/cr.yaml 
 
 sleep 30
+
+oc adm new-project knativetutorial
+oc adm policy add-role-to-user admin developer -n knativetutorial
--- 01-serving/set-env.sh
+++ 01-serving/set-env.sh
@@ -6,5 +6,3 @@ echo "Wait for Knative Serving Pods are up and Running"
 
 sleep 150; while echo && oc get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 20; done
 
-oc adm new-project knativetutorial
-oc adm policy add-role-to-user admin developer -n knativetutorial
